22af7d198e20040ed50743702566674c5991ecd1
Martin Sillaste <martin.sillaste@tktk.ee>
2024-09-28 22:14:14 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter your age");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+//Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if(isAgeNumber == true)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use instagram.");
+    }
+}
+else
+{
+    Console.WriteLine($"{userAge} is not a number. Please try again.");
+}
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Console.WriteLine("Please select your gender (m/f)");
+
+char userGender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("What is your last name?");
+
+string lastName = Console.ReadLine();
+
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {lastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {lastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {lastName}!");
+}
+
+
